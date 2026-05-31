7885ee0fb869ee96d42b34d32440098a61867c5d
szwarcu <jan.szwarc.zeg@gmail.com>
2026-06-01 00:14:19 +0200

    Naprawa sciezki CSS

--- admin/admin.php
+++ admin/admin.php
@@ -13,7 +13,7 @@ $product_count = $conn->query('SELECT COUNT(*) AS total FROM products')->fetch_a
   <title>Panel admina - Zegowska Szama</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet"/>
-  <link href="css/style.css" rel="stylesheet"/>
+  <link href="../css/style.css" rel="stylesheet"/>
 </head>
 <body>
 
