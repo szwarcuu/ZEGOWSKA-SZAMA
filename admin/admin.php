623261bc94e81c513631db90616f65043c6b92d7
szwarcu <jan.szwarc.zeg@gmail.com>
2026-06-01 00:13:06 +0200

    Naprawa sciezki w pliku admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'includes/auth.php';
+require_once '../includes/auth.php';
 require_admin();
-require_once 'includes/db.php';
+require_once '../includes/db.php';
 
 $product_count = $conn->query('SELECT COUNT(*) AS total FROM products')->fetch_assoc()['total'];
 ?>
